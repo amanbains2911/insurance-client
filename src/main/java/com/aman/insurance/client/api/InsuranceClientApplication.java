95980edcaa4c22a28c595a205d5cb2c66f6fa950
amanbains2911 <64704147+amanbains2911@users.noreply.github.com>
2020-06-04 17:03:50 -0400

    Initial commit

---
+++ src/main/java/com/aman/insurance/client/api/InsuranceClientApplication.java
@@ -0,0 +1,44 @@
+package com.aman.insurance.client.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+@RefreshScope
+public class InsuranceClientApplication {
+	
+	@Autowired
+	@Lazy
+	private RestTemplate template;
+	
+	@Value("${insurance.provider.url}")
+	private String url;
+	
+	@GetMapping("/getPlan")
+	public List<String> getPlans(){
+		@SuppressWarnings("unchecked")
+		List<String> plans=template.getForObject(url, List.class);
+		return plans;
+	}
+	
+	@Bean
+	public RestTemplate template() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aman/insurance/client/api/InsuranceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.insurance.client.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
